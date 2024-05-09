bc7effb0201bd23a6e6bbf9a84bd98cbe47e9303
Josh-dev69 <ebubejoshua69@gmail.com>
2024-05-09 14:33:09 +0100

    JavaScript Tutorial

--- JavaScript/Day_02(String Interpolation)/js/exercise3.js
+++ JavaScript/Day_02(String Interpolation)/js/exercise3.js
@@ -7,3 +7,12 @@ console.log(sentence2.match(/because/gi).length); // 3
 const sentence3 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 const cleanedSentence = sentence3.replace(/[^\w\s]/g, ''); // remove all special characters
 console.log(cleanedSentence);
+
+const text = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+const numbers = text.match(/\d+/g); // extract numbers from the text
+console.log(numbers);
+num1 = parseInt(numbers[0]);
+num2 = parseInt(numbers[1]);
+num3 = parseInt(numbers[2]);
+let totalIncome = num1 + num2 + num3;
+console.log(totalIncome);
